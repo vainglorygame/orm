b9eca4133801fb0e119c60ac3a123d43e04e9651
schneefux <schneefux+commit@schneefux.xyz>
2017-04-02 13:16:07 +0200

    add item mappings

---
+++ items.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+"use strict";
+
+var item_source = require("./items_source.js");
+var items = {}
+
+item_source.map((entry) => {
+    items[entry.serverName] = entry.name;
+    items[entry.name] = entry.name;
+});
+
+module.exports = items;
