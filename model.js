356bf1d795bd43d6ff7da5bcd1722602867d3cff
schneefux <schneefux+commit@schneefux.xyz>
2017-04-02 19:42:42 +0200

    remove relationships

--- model.js
+++ model.js
@@ -12,13 +12,5 @@ module.exports = (seq, Seq) => {
         PlayerExt = seq.import("./models/player_ext.js"),
         Item = seq.import("./models/item.js");
 
-    Match.hasMany(Asset, {as: "Assets"});
-    Asset.belongsTo(Match);
-    Roster.belongsTo(Match, { foreignKey: "match_api_id", targetKey: "api_id" });
-    Participant.belongsTo(Roster, { foreignKey: "roster_api_id", targetKey: "api_id" });
-    Participant.belongsTo(Player, { foreignKey: "player_api_id", targetKey: "api_id" });
-    ParticipantItemUse.belongsTo(Participant, { foreignKey: "participant_api_id", targetKey: "api_id" });
-    PlayerExt.belongsTo(Player, { foreignKey: "player_api_id", targetKey: "api_id" });
-
     return {Match, Roster, Participant, ParticipantExt, ParticipantItemUse, Player, PlayerExt, Asset, Item};
 };
