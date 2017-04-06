6d9111d80dc2d42072dc6f978583bdefd357ce5e
schneefux <schneefux+commit@schneefux.xyz>
2017-04-06 13:42:58 +0200

    prepare relations and schema for cruncher

--- model.js
+++ model.js
@@ -1,19 +1,43 @@
 /* jshint esnext:true */
-'use strict';
+"use strict";
 
 module.exports = (seq, Seq) => {
-    let Match = seq.import("./models/match.js"),
+    let // core data
+        Match = seq.import("./models/match.js"),
         Asset = seq.import("./models/asset.js"),
         Roster = seq.import("./models/roster.js"),
         Participant = seq.import("./models/participant.js"),
-        ParticipantExt = seq.import("./models/participant_ext.js"),
-        ItemParticipant = seq.import("./models/item_participant.js"),
         Player = seq.import("./models/player.js"),
+
+        // mappings
+        Item = seq.import("./models/item.js"),
+
+        // Telemetry
+        ItemParticipant = seq.import("./models/item_participant.js"),
+
+        // data stats
+        ParticipantExt = seq.import("./models/participant_ext.js"),
         PlayerExt = seq.import("./models/player_ext.js"),
-        Item = seq.import("./models/item.js");
+
+        // stats mappings
+        StatsDimensions = seq.import("./models/stats_dimensions.js"),
+        Heros = seq.import("./models/heros.js"),
+
+        // hero stats
+        HeroStats = seq.import("./models/hero_stats.js"),
+        HeroDimension = seq.import("./models/hero_dimension.js");
+
 
     Roster.belongsTo(Match, { foreignKey: "match_api_id", targetKey: "api_id" });
     Participant.belongsTo(Roster, { foreignKey: "roster_api_id", targetKey: "api_id" });
+    Participant.belongsTo(Heros, { foreignKey: "actor", targetKey: "api_name" });
 
-    return {Match, Roster, Participant, ParticipantExt, ItemParticipant, Player, PlayerExt, Asset, Item};
+    return {
+        Match, Roster, Participant, Player, Asset,
+        Item,
+        ItemParticipant,
+        ParticipantExt, PlayerExt,
+        StatsDimensions, Heros,
+        HeroStats, HeroDimension
+    };
 };
