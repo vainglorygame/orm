60154a734c5a5e86f48b82db852ce87890f1c624
schneefux <schneefux+commit@schneefux.xyz>
2017-04-15 17:52:02 +0200

    rm default timestamps, generator broke these

--- models/asset.js
+++ models/asset.js
@@ -35,8 +35,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     created_at: {
       type: DataTypes.TIME,
-      allowNull: false,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      allowNull: false
     },
     description: {
       type: DataTypes.STRING(191),
--- models/participant.js
+++ models/participant.js
@@ -51,8 +51,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     created_at: {
       type: DataTypes.TIME,
-      allowNull: false,
-      defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      allowNull: false
     },
     actor: {
       type: DataTypes.STRING(16),
