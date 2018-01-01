475ef5a60f4c7c9bd67c9e3ba1055131eb42728c
schneefux <schneefux+commit@schneefux.xyz>
2018-01-01 15:22:24 +0100

    reimport

--- models/participant_phases.js
+++ models/participant_phases.js
@@ -28,6 +28,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.CHAR(36),
       allowNull: false
     },
+    match_api_id: {
+      type: DataTypes.CHAR(36),
+      allowNull: false
+    },
     dmg_true: {
       type: "DOUBLE",
       allowNull: true
