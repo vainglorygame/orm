08d342b442eac0827c641270a5c49754116d20df
schneefux <schneefux+commit@schneefux.xyz>
2018-01-27 14:55:08 +0100

    add more 5v5 mappings

--- mappings_source.js
+++ mappings_source.js
@@ -645,7 +645,14 @@ module.exports = {
     "*Item_TotemEnhancer*": "Boosted Cam",
     "*Item_TotemBuffer*": "Buffed Cam",
     "*Item_FlareGun_Beta*": "Flare Gun beta",
-    "*Item_WarpGenerator*": "Teleporter Boots",
+    "*Item_WarpGenerator*": "Teleport Boots",
+    "Vision Totem": "Scout Cam",
+    "Totem Multiplier": "Multi-Cam",
+    "Totem Enhancer": "Boosted Cam",
+    "Totem Buffer": "Buffed Cam",
+    "Flare Gun Beta": "Flare Gun beta",
+    "Warp Generator": "Teleport Boots",
     "5v5_pvp_casual": "5v5_casual",
-    "5v5_pvp_ranked": "5v5_ranked"
+    "5v5_pvp_ranked": "5v5_ranked",
+    "private_party_vg_5v5": "priv_5v5_blind"
 }
